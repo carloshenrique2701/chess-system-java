d2795ddc7847e5d01d6c2c95a0fa079084e7e474
Carlos Henrique <carloshrr1209@gmail.com>
2026-06-29 10:15:43 -0300

    Possible movies of a piece

--- src/application/UI.java
+++ src/application/UI.java
@@ -45,7 +45,6 @@ public class UI {
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
-
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
             for (int j = 0; j < pieces.length; j++) {
@@ -54,7 +53,6 @@ public class UI {
             System.out.println();
         }
         System.out.println("  a b c d e f g h");
-
     }
 
     private static void printPiece(ChessPiece piece) {
